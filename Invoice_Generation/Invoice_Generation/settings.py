e2706d34e41d3b45956e0c0c4173934802da5d21
JararAsif <110182865+JararAsif@users.noreply.github.com>
2025-07-10 19:49:05 +0500

    Update settings.py

--- Invoice_Generation/Invoice_Generation/settings.py
+++ Invoice_Generation/Invoice_Generation/settings.py
@@ -144,6 +144,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'jararvirk10@gmail.com'         # Your Gmail
-EMAIL_HOST_PASSWORD = 'diawbsnzvrledyxp'                # Your actual App Password
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
